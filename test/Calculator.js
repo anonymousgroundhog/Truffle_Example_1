f744210b73869fd74b6e5bf21caf9fbabadaac9f
anonymousgroundhog <leapfrogopus@gmail.com>
2022-10-26 16:10:40 -0400

    Adding test

---
+++ test/Calculator.js
@@ -0,0 +1,27 @@
+const Calculator = artifacts.require('Calculator.sol');
+
+contract('Calculator', () => {
+	it('should add data correctly', async () => {
+		const calculatorInstance = await Calculator.deployed();
+		addition_result = await calculatorInstance.add(2,2);
+		assert(addition_result == 4);
+	});
+
+	it('should subtract data correctly', async () => {
+		const calculatorInstance = await Calculator.deployed();
+		addition_result = await calculatorInstance.subtract(4,2);
+		assert(addition_result == 2);
+	});
+
+	it('should multiply data correctly', async () => {
+		const calculatorInstance = await Calculator.deployed();
+		addition_result = await calculatorInstance.multiply(4,2);
+		assert(addition_result == 8);
+	});
+
+	it('should divide data correctly', async () => {
+		const calculatorInstance = await Calculator.deployed();
+		addition_result = await calculatorInstance.divide(4,2);
+		assert(addition_result == 2);
+	});
+});
