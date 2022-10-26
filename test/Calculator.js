e8f5d681b16518852518baa666ce82e35b615ca9
anonymousgroundhog <leapfrogopus@gmail.com>
2022-10-26 16:21:27 -0400

    Adding testing update

--- test/Calculator.js
+++ test/Calculator.js
@@ -4,24 +4,24 @@ contract('Calculator', () => {
 	it('should add data correctly', async () => {
 		const calculatorInstance = await Calculator.deployed();
 		addition_result = await calculatorInstance.add(2,2);
-		assert(addition_result.toNumber() === 4);
+		assert(addition_result.toString() === '4');
 	});
 
 	it('should subtract data correctly', async () => {
 		const calculatorInstance = await Calculator.deployed();
 		addition_result = await calculatorInstance.subtract(4,2);
-		assert(addition_result.toNumber() === 2);
+		assert(addition_result.toString() === '2');
 	});
 
 	it('should multiply data correctly', async () => {
 		const calculatorInstance = await Calculator.deployed();
 		addition_result = await calculatorInstance.multiply(4,2);
-		assert(addition_result.toNumber() === 8);
+		assert(addition_result.toString() === '8');
 	});
 
 	it('should divide data correctly', async () => {
 		const calculatorInstance = await Calculator.deployed();
 		addition_result = await calculatorInstance.divide(4,2);
-		assert(addition_result.toNumber() === 2);
+		assert(addition_result.toString() === '2');
 	});
 });
